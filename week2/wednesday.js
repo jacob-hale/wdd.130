521213d7c548dca2d4b48c2e02772f9503d0d479
Jacob Hale <Jchale23@byui.edu>
2022-09-28 19:38:05 -0600

    everything

---
+++ week2/wednesday.js
@@ -0,0 +1,8 @@
+function changeText() {
+    let newText = "hi there"
+    let oldText = "Watch me whip"
+    let pageText = document.getElementById('divWithText').innerHTML
+    if (pageText == newText){
+    document.getElementById('divWithText').innerHTML = oldText;}
+    else
+}
